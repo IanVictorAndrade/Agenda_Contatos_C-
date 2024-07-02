3bf9c2724671240893c498dd2db5ac49ae654a3d
ian <ian.barbosa@tjro.jus.br>
2024-07-02 12:32:41 -0400

    refac: reorganizando construtor do context do banco

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -5,9 +5,7 @@ namespace MeuSiteEmMVC.Data
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions options) : base(options)
-        {
-        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
         public DbSet<ContatoModel> Contato { get; set; }
         // public DbSet<UsuarioModel> Users { get; set; }
