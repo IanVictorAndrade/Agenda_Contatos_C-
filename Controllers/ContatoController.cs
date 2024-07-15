a10839a3d0d847d2facc57cd87ae45fc85cdbe9d
ian <ian.barbosa@tjro.jus.br>
2024-07-15 08:38:55 -0400

    feat: adição de datatable com jquery

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,6 +1,7 @@
 using MeuSiteEmMVC.Models;
 using MeuSiteEmMVC.Repositorio;
 using Microsoft.AspNetCore.Mvc;
+
 namespace MeuSiteEmMVC.Controllers;
 
 public class ContatoController : Controller
@@ -34,21 +35,57 @@ public class ContatoController : Controller
     [HttpPost] 
     public IActionResult Criar(ContatoModel? contato)
     {
-        _contatoRepositorio.Adicionar(contato);
-        return RedirectToAction("Index");
+        try
+        {
+            if (!ModelState.IsValid) return View(contato);
+            _contatoRepositorio.Adicionar(contato);
+            TempData["MensagemSucesso"] = "Contato adicionado com sucesso!";
+            return RedirectToAction("Index");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            TempData["MensagemErro"] = $"Erro ao adicionar contato! detalhes: {e.Message}";
+            return RedirectToAction("Index");
+        }
     }
     
     [HttpPost] 
     public IActionResult Alterar(ContatoModel contato)
     {
-        _contatoRepositorio.Atualizar(contato);
-        return RedirectToAction("Index");
+        try
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index");
+            _contatoRepositorio.Atualizar(contato);
+            TempData["MensagemSucesso"] = "Contato alterado com sucesso!";
+            return RedirectToAction("Index");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            TempData["MensagemErro"] = $"Erro ao alterar contato! detalhes: {e.Message}";
+            return RedirectToAction("Index");
+        }
     }
     
     [HttpGet]
     public IActionResult Apagar(int id)
     {
-        _contatoRepositorio.Remover(id);
-        return RedirectToAction("Index");
+        try
+        {
+            bool apagado = _contatoRepositorio.Remover(id);
+            if (!apagado) TempData["MensagemErro"] = "Não foi possível remover o contato!";
+            
+            TempData["MensagemSucesso"] = "Contato removido com sucesso!";
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            TempData["MensagemErro"] = $"Erro ao remover contato! detalhes: {e.Message}";
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/ContatoModel.cs
+++ Models/ContatoModel.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeuSiteEmMVC.Models;
 
 public class ContatoModel {
     
     public int Id { get; set; }
+    [Required(ErrorMessage = "Digite o nome do contato")]
     public string Nome { get; set; }
+    [Required(ErrorMessage = "Digite o email do contato")]
+    [EmailAddress(ErrorMessage = "Digite um email válido")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "Digite o telefone do contato")]
+    [Phone(ErrorMessage = "Digite um telefone válido")]
     public string Telefone { get; set; }
 }
